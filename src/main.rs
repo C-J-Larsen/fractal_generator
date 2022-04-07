a499d08780ed674265a98015d8a37ed66b26aeeb
Dr-Tronic <cjlar2002@gmail.com>
2022-04-06 23:18:20 -0600

    Added capability to specify negative numbers on the command line with '~'.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ use fractals::{Fractal, FractalType};
 extern crate clap;
 use clap::{Command, Arg, Values};
 
+fn str_to_val<T: std::str::FromStr>(inpStr: &str) -> T
+where
+    T: std::str::FromStr, <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let new_inp_str: String = inpStr.replace("~", "-");
+    new_inp_str.parse::<T>().unwrap()
+}
+
 fn main() {
     // Parse the command line arguments
     let matches = Command::new(env!("CARGO_PKG_NAME"))
@@ -63,8 +71,8 @@ fn main() {
      * Pull the information needed from the command line arguments
      *************************************************************************/
     // Create the object that will handle making the BMP image
-    let width:     u32  = matches.value_of("width").unwrap().parse().unwrap();
-    let height:    u32  = matches.value_of("height").unwrap().parse().unwrap();
+    let width:     u32  = str_to_val(matches.value_of("width").unwrap());
+    let height:    u32  = str_to_val(matches.value_of("height").unwrap());
     let file_name: &str = matches.value_of("file-name").unwrap();
     let bmp_img_obj = BMPImg::new(width, height, file_name);
 
@@ -76,23 +84,23 @@ fn main() {
             if frac_name == "Mandelbrot" {
                 FractalType::Mandelbrot ()
             } else if frac_name == "Julia" {
-                let real_seed: f32 = frac_matches.values_of("real").unwrap().next().unwrap().parse().unwrap();
-                let imag_seed: f32 = frac_matches.values_of("imaginary").unwrap().next().unwrap().parse().unwrap();
+                let real_seed: f32 = str_to_val(frac_matches.values_of("real").unwrap().next().unwrap());
+                let imag_seed: f32 = str_to_val(frac_matches.values_of("imaginary").unwrap().next().unwrap());
                 FractalType::Julia (MyComplex::new(real_seed, imag_seed))
             } else { // Must be a Newton fractal
                 // If a Newton fractal, get the values of each of the roots.
                 let mut roots: Vec<MyComplex<f32>> = Vec::new();
 
-                loop {
+                'root: loop {
                     let real: f32;
                     let imag: f32;
                     match frac_matches.values_of("root").unwrap().next() {
-                        Some(real_str) => real = real_str.parse().unwrap(),
-                        None => break,
+                        Some(real_str) => real = str_to_val(real_str),
+                        None => break 'root,
                     }
                     match frac_matches.values_of("root").unwrap().next() {
-                        Some(imag_str) => imag = imag_str.parse().unwrap(),
-                        None => break,
+                        Some(imag_str) => imag = str_to_val(imag_str),
+                        None => break 'root,
                     }
                     roots.push(MyComplex::new(real, imag));
                 }
@@ -104,7 +112,7 @@ fn main() {
 
     // Put together the fractal object
     let frac_obj: &Fractal = &Fractal::new(
-        matches.value_of("max-iters").unwrap().parse().unwrap(),
+        str_to_val(matches.value_of("max-iters").unwrap()),
         frac_kind,
     );
 
@@ -117,12 +125,12 @@ fn main() {
     let height_as_f32: f32 = height as f32;
 
     let mut r_inputs: Values = matches.values_of("real-range").unwrap();
-    let r_start: f32 = r_inputs.next().unwrap().parse().unwrap();
-    let r_range: f32 = r_inputs.next().unwrap().parse::<f32>().unwrap() - r_start;
+    let r_start: f32 = str_to_val(r_inputs.next().unwrap());
+    let r_range: f32 = str_to_val::<f32>(r_inputs.next().unwrap()) - r_start;
 
     let mut i_inputs: Values = matches.values_of("imag-range").unwrap();
-    let i_start: f32 = i_inputs.next().unwrap().parse().unwrap();
-    let i_range: f32 = i_inputs.next().unwrap().parse::<f32>().unwrap() - i_start;
+    let i_start: f32 = str_to_val(i_inputs.next().unwrap());
+    let i_range: f32 = str_to_val::<f32>(i_inputs.next().unwrap()) - i_start;
 
     // Create the closure that colors each pixel
     let pix_pos_to_color = |pix_pos: graphics::PixPos| -> graphics::Color {
